c9c391adfc93a3b0880b1a87b40c22dc9614a71a
Chas Nelson <cvs@chasnelson.co.uk>
2020-04-07 11:16:06 +0100

    build stream based on miguelgriberg's blog https://blog.miguelgrinberg.com/post/video-streaming-with-flask

--- open_optical_gating/app/app.py
+++ open_optical_gating/app/app.py
@@ -1,64 +1,90 @@
-# /usr/bin/python3
+#!/usr/bin/env python
 
+"""Web app for interacting with the open optical gating system.
+Much of this was inspired by https://github.com/miguelgrinberg/flask-video-streaming"""
 
-# Python imports
-from flask import Flask, render_template, Response, request
-import picamera
-import numpy as np
-import cv2
+# Imports
+import os
+from importlib import import_module
+from flask import Flask, render_template, Response
 
-# Local imports
-import cli as tb
+# import camera driver
+from camera_pi import Camera
 
+#
+import cli
 
-# Defines app instance
 app = Flask(__name__)
-vc = cv2.VideoCapture(0)
 
 
 @app.route("/")
-def initialise():
-    return render_template("capture.html")
+def index():
+    """Video streaming home page."""
+    return render_template("index.html")
 
 
-def gen():
-
+def gen(camera):
+    """Video streaming generator function."""
     while True:
-
-        rval, frame = vc.read()
-        cv2.imwrite("pic.jpg", frame)
-        yield (
-            b"--frame\r\n"
-            b"Content-Type: image/jpeg\r\n\r\n" + open("pic.jpg", "rb").read() + b"\r\n"
-        )
+        frame = camera.get_frame()
+        yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
 
 @app.route("/video_feed")
 def video_feed():
-    return Response(gen(), mimetype="multipart/x-mixed-replace; boundary=frame")
-
-
-@app.route("/test")
-def tests():
-
-    # Defines initial variables
-    fps_max, pi_res = tb.check_fps()
-
-    return render_template("test.html", fps_max=fps_max, pi_res=pi_res)
-
-@app.route("/emulate_data_capture")
-def emulate_data_capture():
-    tb.emulate_data_capture()
-    return "Nothing"
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 if __name__ == "__main__":
+    app.run(debug=True, host="192.168.0.13", threaded=True)
 
-    # Initialises the camera
-    # 	camera = picamera.PiCamera()
-    # 	camera.resolution = (100,100)
-    # 	camera.framerate = 24
-    # 	frame = np.empty((100,100,3), dtype=np.uint8)
 
-    app.run()
+# vc = cv2.VideoCapture(0)
 
+
+# @app.route("/")
+# def initialise():
+#     return render_template("capture.html")
+
+
+# def gen():
+
+#     while True:
+
+#         rval, frame = vc.read()
+#         cv2.imwrite("pic.jpg", frame)
+#         yield (
+#             b"--frame\r\n"
+#             b"Content-Type: image/jpeg\r\n\r\n" + open("pic.jpg", "rb").read() + b"\r\n"
+#         )
+
+
+# @app.route("/video_feed")
+# def video_feed():
+#     return Response(gen(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+# @app.route("/test")
+# def tests():
+
+#     # Defines initial variables
+#     fps_max, pi_res = tb.check_fps()
+
+#     return render_template("test.html", fps_max=fps_max, pi_res=pi_res)
+
+# @app.route("/emulate_data_capture")
+# def emulate_data_capture():
+#     tb.emulate_data_capture()
+#     return "Nothing"
+
+
+# if __name__ == "__main__":
+
+#     # Initialises the camera
+#     # 	camera = picamera.PiCamera()
+#     # 	camera.resolution = (100,100)
+#     # 	camera.framerate = 24
+#     # 	frame = np.empty((100,100,3), dtype=np.uint8)
+
+#     app.run()
