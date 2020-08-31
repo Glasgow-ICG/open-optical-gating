d26630eac2dd5d3ee01c768ba59df7dc5f49bd2e
Chas Nelson <cvs@chasnelson.co.uk>
2020-08-31 11:09:08 +0100

    make file_optical_gater align with websocket

--- open_optical_gating/cli/file_optical_gater.py
+++ open_optical_gating/cli/file_optical_gater.py
@@ -1,4 +1,4 @@
-"""Extension of CLI Open Optical Gating System for Emulation"""
+"""Extension of CLI Open Optical Gating System for emulating gating with saved brightfield data"""
 
 # Python imports
 import sys
@@ -10,12 +10,12 @@ from loguru import logger
 from skimage import io
 
 # Local imports
-import open_optical_gating.cli.pixelarray as pa
 import open_optical_gating.cli.optical_gater_server as server
+import open_optical_gating.cli.pixelarray as pa
 
 
 class FileOpticalGater(server.OpticalGater):
-    """Extends the optical gater server for a data file.
+    """Extends the optical gater server for a pre-captured data file.
     """
 
     def __init__(
@@ -24,35 +24,64 @@ class FileOpticalGater(server.OpticalGater):
         settings=None,
         ref_frames=None,
         ref_frame_period=None,
-        repeats=1
+        repeats=1,
+        automatic_target_frame=True,
     ):
         """Function inputs:
-            file_source_path   str   Path to file we will read as our input
-            settings           dict  Parameters affecting operation (see default_settings.json)
-            repeats            int   Number of times to repeat the frames in the source file.
-                                     It may be useful to run through the frames more than once,
-                                     for more sustained testing.
+            settings      dict  Parameters affecting operation (see default_settings.json)
         """
 
-        # initialise parent
+        # Get the assumed brightfield framerate from the settings
+        self.framerate = settings["brightfield_framerate"]
+
+        # Initialise parent
         super(FileOpticalGater, self).__init__(
             settings=settings, ref_frames=ref_frames, ref_frame_period=ref_frame_period,
         )
+
+        # Load the data
         self.load_data(file_source_path)
-        self.next_frame_index = 0
-        self.last_frame_wallclock_time = None
+
+        # How many times to repeat the sequence
         self.repeats_remaining = repeats
-        # By default we will ask user for their preferred initial target frame
-        self.automatic_target_frame = False
+
+        # By default we will take a guess at a goof target frame (True)
+        # rather than ask user for their preferred initial target frame (False)
+        self.automatic_target_frame = automatic_target_frame
 
     def load_data(self, filename):
-        """Apply data source-related settings."""
-        # Data-source settings
+        """Load data file"""
+        # Load
         logger.success("Loading image data...")
         self.data = io.imread(filename)
         self.height, self.width = self.data[0].shape
-        self.framerate = self.settings["brightfield_framerate"]
+
+        # Initialise frame iterator and time tracker
+        self.next_frame_index = 0
         self.start_time = time.time()  # we use this to sanitise our timestamps
+        self.last_frame_wallclock_time = None
+
+    def run_server(self, force_framerate=False):
+        """ Run the OpticalGater server, acting on the in-file data.
+            Function inputs:
+                force_framerate bool    Whether or not to slow down the compute time to emulate real-world speeds
+        """
+        if self.automatic_target_frame == False:
+            logger.success("Determining reference period...")
+            while self.state != "sync":
+                while not self.stop:
+                    self.analyze_pixelarray(self.next_frame(force_framerate=True))
+                logger.info("Requesting user input...")
+                self.user_select_ref_frame()
+            logger.success(
+                "Period determined ({0} frames long) and user has selected frame {1} as target.",
+                self.pog_settings["reference_period"],
+                self.pog_settings["referenceFrame"],
+            )
+
+        logger.success("Emulating...")
+        while not self.stop:
+            self.analyze_pixelarray(self.next_frame(force_framerate=force_framerate))
 
     def next_frame(self, force_framerate=False):
         """This function gets the next frame from the data source, which can be passed to analyze()"""
@@ -66,11 +95,14 @@ class FileOpticalGater(server.OpticalGater):
                 # the 1e-9 is a very small time to allow for the calculation
                 time.sleep(wait_s - 1e-9)
             else:
-                logger.warning('Failing to sustain requested framerate {0}fps for frame {1} (requested negative delay {2}s)'.format(
-                    self.settings["brightfield_framerate"],
-                    self.next_frame_index,
-                    wait_s))
-    
+                logger.warning(
+                    "Failing to sustain requested framerate {0}fps for frame {1} (requested negative delay {2}s)".format(
+                        self.settings["brightfield_framerate"],
+                        self.next_frame_index,
+                        wait_s,
+                    )
+                )
+
         if self.next_frame_index == self.data.shape[0] - 1:
             self.repeats_remaining -= 1
             if self.repeats_remaining <= 0:
@@ -79,7 +111,7 @@ class FileOpticalGater(server.OpticalGater):
             else:
                 # Start again at the first frame in the file
                 self.next_frame_index = 0
-        
+
         next = pa.PixelArray(
             self.data[self.next_frame_index, :, :],
             metadata={
@@ -92,25 +124,15 @@ class FileOpticalGater(server.OpticalGater):
 
 
 def run(settings):
-    """Emulated data capture for a set of sample brightfield frames."""
     logger.success("Initialising gater...")
-    analyser = FileOpticalGater(file_source_path=settings["path"], settings=settings,)
-
-    logger.success("Determining reference period...")
-    while analyser.state != "sync":
-        while not analyser.stop:
-            analyser.analyze_pixelarray(analyser.next_frame(force_framerate=True))
-        logger.info("Requesting user input...")
-        analyser.user_select_ref_frame(10)
-    logger.success(
-        "Period determined ({0} frames long) and user has selected frame {1} as target.",
-        analyser.pog_settings["reference_period"],
-        analyser.pog_settings["referenceFrame"],
+    analyser = FileOpticalGater(
+        file_source_path=settings["path"],
+        settings=settings,
+        automatic_target_frame=False,
     )
 
-    logger.success("Emulating...")
-    while not analyser.stop:
-        analyser.analyze_pixelarray(analyser.next_frame(force_framerate=True))
+    logger.success("Running server...")
+    analyser.run_server(force_framerate=True)
 
     logger.success("Plotting summaries...")
     analyser.plot_triggers()
@@ -118,11 +140,8 @@ def run(settings):
     analyser.plot_accuracy()
     analyser.plot_running()
 
-    logger.success("Fin.")
-
 
 if __name__ == "__main__":
-    t = time.time()
     # Reads data from settings json file
     if len(sys.argv) > 1:
         settings_file = sys.argv[1]
@@ -132,6 +151,5 @@ if __name__ == "__main__":
     with open(settings_file) as data_file:
         settings = json.load(data_file)
 
-    # Performs an emulated data capture
+    # Runs the server
     run(settings)
-    print(time.time() - t)
