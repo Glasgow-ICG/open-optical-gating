3ef3d33bf089c9aefbaff137051b9669c0991cf2
Jonny Taylor <jonny@Jonnys-MacBook-Pro.local>
2020-11-13 18:42:58 +0000

    Seems to need a setup.py for direct install from pip

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+                 name="open-optical-gating", # Replace with your own username
+                 version="2.0.0b",
+                 author="Alex Drysdale, Patrick Cameron, Jonathan Taylor and Chas Nelson",
+                 author_email="",
+                 description="Open-source prospective and adaptive optical gating for 3D fluorescence microscopy of beating hearts",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/Glasgow-ICG/open-optical-gating/",
+                 packages=setuptools.find_packages(),
+                 classifiers=[
+                              "Programming Language :: Python :: 3",
+                              "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                              "Operating System :: OS Independent",
+                              ],
+                 python_requires='>=3.7',
+                 )
