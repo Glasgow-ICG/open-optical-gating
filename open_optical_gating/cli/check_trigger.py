fe65127e2f4352c6bc87a7e5dcf595699972374a
Cameron <cam.jw@outlook.com>
2022-01-31 10:17:49 +0000

    bring check_trigger up to date with new settings format

--- open_optical_gating/cli/check_trigger.py
+++ open_optical_gating/cli/check_trigger.py
@@ -2,36 +2,38 @@
 
 # Python imports
 import time
-import cli
 import json
 import sys
-
 from loguru import logger
+
+from . import pi_optical_gater as piog
+from . import file_optical_gater as fog
 
 logger.remove()
 logger.add(sys.stderr, level="DEBUG")
 # logger.add("oog_{time}.log", level="DEBUG")
 logger.enable("open_optical_gating")
 
-def run(args, desc, delay=500e3, number=10):
+def run(args, desc, delay = 5, number = 10, wait = 1):
     '''
         Run the optical gater based on a settings.json file which includes
         the path to the .tif file to be processed.
         
         Params:   raw_args   list    Caller should normally pass sys.argv here
                   desc       str     Description to provide as command line help description
-                  delay      float
-                  number     int
+                  delay      float   Time (seconds) at which trigger should be sent
+                  number     int     Number of triggers to be sent
+                  wait       float   Time (seconds) to wait between triggers
         '''
-    settings = load_settings(args, desc)["trigger"]
-    laser_trigger_pin = settings["laser_trigger_pin"]
-    fluorescence_camera_pins = settings["fluorescence_camera_pins"]
+    settings = fog.load_settings(args, desc)
+    laser_trigger_pin = settings["trigger"]["laser_trigger_pin"]
+    fluorescence_camera_pins = settings["trigger"]["fluorescence_camera_pins"]
     
     # us, this is the duration of a laser pulse (also controls the camera exposure if set to do so)
-    duration = settings["fluorescence_exposure_us"]  
+    duration = settings["trigger"]["fluorescence_exposure_us"]  
     
     # Create pi optical gater server
-    server = pi_optical_gater.PiOpticalGater(settings=dict_data)
+    server = piog.PiOpticalGater(settings = settings)
 
     # Tests laser and camera
     for i in range(number):
@@ -40,6 +42,7 @@ def run(args, desc, delay=500e3, number=10):
             delay
         )
         logger.info("Took {0} s", time.time()-t)
+        time.sleep(wait)
 
     return True
 
